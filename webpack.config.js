27b8716d5be463402032e1de206c975570f691b3
papa <papa00103@naver.com>
2022-05-22 19:14:21 +0900

    :technologist: Use vercel dev command (#5)
    To use vercel serverless function in development env

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = (env, options) => {
   console.log(env, options)
@@ -56,6 +57,7 @@ module.exports = (env, options) => {
       ]
     },
     plugins: [
+      new webpack.EnvironmentPlugin({ NODE_ENV: options.mode, DEBUG: false }),
       new VueLoaderPlugin(),
       new HtmlPlugin({
         template: './src/index.html'
